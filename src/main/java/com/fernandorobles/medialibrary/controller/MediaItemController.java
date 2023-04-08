19b3553e4f552045e9d10aa5ffd63e242a952a37
fernando robles <advancexz@gmail.com>
2023-04-07 17:56:05 -0700

    - CrossOrigin added to support CORS on localhost 4200

--- src/main/java/com/fernandorobles/medialibrary/controller/MediaItemController.java
+++ src/main/java/com/fernandorobles/medialibrary/controller/MediaItemController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/media")
 @Slf4j
 public class MediaItemController {
